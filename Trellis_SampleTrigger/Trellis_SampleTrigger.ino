e208d8eedd56559d7b470277ebf24b0280315662
mtgNic <nic.jsb@gmail.com>
2015-04-29 19:49:00 -0400

    Trellis EventDelay added sample trigger

--- Trellis_SampleTrigger/Trellis_SampleTrigger.ino
+++ Trellis_SampleTrigger/Trellis_SampleTrigger.ino
@@ -1,38 +1,39 @@
 /*
    Example using a piezo to trigger an audio sample to play,
-   and a knob to set the playback pitch,
-   with Mozzi sonification library.
+ and a knob to set the playback pitch,
+ with Mozzi sonification library.
  
-   Demonstrates one-shot samples and analog input for control.
-   
-   This example goes with a tutorial on the Mozzi site:
-   http://sensorium.github.io/Mozzi/learn/introductory-tutorial/
-  
-   The circuit:
-     Audio output on digital pin 9 on a Uno or similar, or
-    DAC/A14 on Teensy 3.0/3.1, or 
-     check the README or http://sensorium.github.com/Mozzi/
-
-     Potentiometer connected to analog pin 0.
-       Center pin of the potentiometer goes to the analog pin.
-       Side pins of the potentiometer go to +5V and ground
+ Demonstrates one-shot samples and analog input for control.
  
-     Piezo on analog pin 3:
-       + connection of the piezo attached to the analog pin
-       - connection of the piezo attached to ground
-       1-megohm resistor between the analog pin and ground
+ This example goes with a tutorial on the Mozzi site:
+ http://sensorium.github.io/Mozzi/learn/introductory-tutorial/
  
-   Mozzi help/discussion/announcements:
-   https://groups.google.com/forum/#!forum/mozzi-users
+ The circuit:
+ Audio output on digital pin 9 on a Uno or similar, or
+ DAC/A14 on Teensy 3.0/3.1, or 
+ check the README or http://sensorium.github.com/Mozzi/
  
-   Tim Barrass 2013.
-   CC by-nc-sa
-*/
+ Potentiometer connected to analog pin 0.
+ Center pin of the potentiometer goes to the analog pin.
+ Side pins of the potentiometer go to +5V and ground
+ 
+ Piezo on analog pin 3:
+ + connection of the piezo attached to the analog pin
+ - connection of the piezo attached to ground
+ 1-megohm resistor between the analog pin and ground
+ 
+ Mozzi help/discussion/announcements:
+ https://groups.google.com/forum/#!forum/mozzi-users
+ 
+ Tim Barrass 2013.
+ CC by-nc-sa
+ */
 
 #include <ADC.h>  // Teensy 3.0/3.1 uncomment this line and install http://github.com/pedvide/ADC
 #include <MozziGuts.h>
 #include <Sample.h> // Sample template
 #include <samples/burroughs1_18649_int8.h> // a converted audio sample included in the Mozzi download
+#include <EventDelay.h>
 
 // Trellis stuff ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #include <i2c_t3.h>
@@ -42,11 +43,11 @@ Adafruit_Trellis matrix = Adafruit_Trellis();
 Adafruit_TrellisSet trellis = Adafruit_TrellisSet(&matrix);
 
 #define NUMTRELLIS 1
-#define numKeyr (NUMTRELLIS * 16)
+#define numKeys (NUMTRELLIS * 16)
 
 #define INTPIN 31
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+EventDelay del;
 
 const char KNOB_PIN = 1;  // set the analog input pin for the knob
 const char PIEZO_PIN = 3;  // set the analog input pin for the piezo 
@@ -59,13 +60,15 @@ float recorded_pitch = (float) BURROUGHS1_18649_SAMPLERATE / (float) BURROUGHS1_
 boolean triggered = false;
 
 void setup(){
-  pinMode(1, INPUT_PULLUP);
+  del.set(30);
   
+  pinMode(1, INPUT_PULLUP);
+
   // Initialize trellis
   pinMode(INTPIN, INPUT);
   digitalWrite(INTPIN, HIGH);
   trellis.begin(0x70); // Trellis address
-  
+
   //Serial.begin(9600); // for Teensy 3.0/3.1, beware printout can cause glitches
   Serial.begin(115200); // set up the Serial output so we can look at the piezo values // set up the Serial output so we can look at the piezo values
   startMozzi(); // :))
@@ -74,15 +77,15 @@ void setup(){
 
 void updateControl(){
   // read the knob
-//  int knob_value = mozziAnalogRead(KNOB_PIN); // value is 0-1023
+  //  int knob_value = mozziAnalogRead(KNOB_PIN); // value is 0-1023
   int knob_value = digitalRead(1);
-  
+
   // map it to values between 0.1 and about double the recorded pitch
   float pitch = (recorded_pitch * (float) 512 / 512.f) + 0.1f; 
-  
+
   // set the sample playback frequency
   aSample.setFreq(pitch);
-  
+
   // read the piezo
   int piezo_value = 512; // value is 0-1023
 
@@ -90,17 +93,21 @@ void updateControl(){
   Serial.print("knob_value = ");
   Serial.print(knob_value);
 
-  if (trellis.readSwitches()) {
+  if (trellis.readSwitches() && del.ready()) {
+    del.start();
     for (uint8_t i=0; i<numKeys; i++) {
-      if (!triggered) {
-        aSample.start();
-       triggered = true; 
+      if (trellis.justPressed(i)) {
+        if (!triggered) {
+          aSample.start();
+          triggered = true; 
+        }  
       }
     } 
-  } else {
+  } 
+  else {
     triggered = false; 
   }
- 
+
   Serial.println(); // next line
 }
 
@@ -113,4 +120,5 @@ int updateAudio(){
 void loop(){
   audioHook();
 }
+
 
