5f9e8afab625bcba31e3b2b881f8be60221b2591
mtgNic <nic.jsb@gmail.com>
2015-05-02 20:14:21 -0400

    master sketch for now

--- aMaster_Sketch/aMaster_Sketch.ino
+++ aMaster_Sketch/aMaster_Sketch.ino
@@ -35,6 +35,23 @@
 #include <samples/burroughs1_18649_int8.h> // a converted audio sample included in the Mozzi download
 #include <EventDelay.h>
 #include "Adafruit_TPA2016.h" //Amp
+#include <LiquidCrystalFast.h>
+#include <Encoder.h>
+
+// Initialize LCDFast with the numbers of the interface pins~~~~~~~~~
+LiquidCrystalFast lcd(14, 15, 16, 20, 21, 22, 23);
+// LCD pins: RS  RW  EN  D4  D5  D6  D7
+
+//Encoder setup~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Encoder knob1(2, 3);
+Encoder knob2(5, 6);
+Encoder knob3(8, 9);
+Encoder knob4(11, 12);
+
+long position1  = -999;
+long position2  = -999;
+long position3  = -999;
+long position4  = -999;
 
 // Declare amp ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Adafruit_TPA2016 audioamp = Adafruit_TPA2016();
@@ -159,9 +176,7 @@ void setup(){
   audioamp.setReleaseControl(0);
   audioamp.setGain(30);
 
-
   aSample.setFreq(recorded_pitch);
-
 
   del.set(15);   // Set EventDelay
 
@@ -185,8 +200,80 @@ void setup(){
   startMozzi();
 }
 
+//Progress bar function~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void LCD_progress_bar (int row, int var, int minVal, int maxVal)
+{
+  int block = map(var, minVal, maxVal, 0, 16);   // Block represent the current LCD space (modify the map setting to fit your LCD)
+  int line = map(var, minVal, maxVal, 0, 80);     // Line represent the theoretical lines that should be printed 
+  int bar = (line-(block*5));                             // Bar represent the actual lines that will be printed
 
+  /* LCD Progress Bar Characters, create your custom bars */
+
+  byte bar1[8] = { 
+    0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10    };
+  byte bar2[8] = { 
+    0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18    };
+  byte bar3[8] = { 
+    0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C    };
+  byte bar4[8] = { 
+    0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E    };
+  byte bar5[8] = { 
+    0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F    };
+  lcd.createChar(1, bar1);
+  lcd.createChar(2, bar2);
+  lcd.createChar(3, bar3);
+  lcd.createChar(4, bar4);
+  lcd.createChar(5, bar5);
+
+  for (int x = 0; x < block; x++)                        // Print all the filled blocks
+  {
+    lcd.setCursor (x, row);
+    lcd.write (1023);
+  }
+
+  lcd.setCursor (block, row);                            // Set the cursor at the current block and print the numbers of line needed
+  if (bar != 0) lcd.write (bar);
+  if (block == 0 && line == 0) lcd.write (1022);   // Unless there is nothing to print, in this case show blank
+
+  for (int x = 16; x > block; x--)                       // Print all the blank blocks
+  {
+    lcd.setCursor (x, row);
+    lcd.write (1022);
+  }
+}
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void updateControl(){
+
+  long new1, new2, new3, new4;
+  new1 = constrain(knob1.read(), -100, 100);
+  new2 = constrain(knob2.read(), -100, 100);
+  new3 = constrain(knob3.read(), -100, 100);
+  new4 = constrain(knob4.read(), -100, 100);
+
+
+  if (new1 != position1) {
+    //update amp 
+    int gainer = map(new1, 100, -100, -28, 30);
+    audioamp.setGain(gainer);
+
+    Serial.println(audioamp.getGain());
+
+    LCD_progress_bar(0, gainer, -28, 30);
+
+    lcd.setCursor(0, 1);
+    lcd.print("Volume"); 
+
+  } 
+  else if(new2 != position2 || new3 != position3 || new4 != position4){
+  }
+
+  //Update Positions
+  position1 = new1;
+  position2 = new2;
+  position3 = new3;
+  position4 = new4;
 
   // Read Trellis input
   if (del.ready()) {
@@ -217,15 +304,15 @@ void updateControl(){
   trellis.writeDisplay(); //Change LEDs
 }
 
-
 int updateAudio() {
   return aSample.next();
 }
 
-
 void loop() {
   audioHook();
 }
+
+
 
 
 
